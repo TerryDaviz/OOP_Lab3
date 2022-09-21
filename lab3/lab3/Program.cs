f62a4fe69465eba69e0d2fcee73519c2ec70370f
Ilya <iltihomsorok@gmail.com>
2022-09-21 12:41:38 +0300

    added overloaded operators

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -16,6 +16,48 @@ namespace lab3
 
         public List<T> queue => this.q;
 
+        public static implicit operator int(Q<T> q1)
+        {
+            return q1.q.Capacity;
+        }
+
+        public static Q<T> operator --(Q<T> q1)
+        {
+            T lastElem = q1.q[q1.q.Count - 1];
+            q1.q.RemoveAt(q1.q.Count - 1);
+            return q1;
+        }
+
+        public static Q<T> operator +(Q<T> q1, T element)
+        {
+            q1.q.Insert(0, element);
+            return new Q<T> { };
+        }
+
+        public static Q<T> operator <(Q<T> q1, Q<T> q2)
+        {
+            q2.q.Sort();
+            q2.q.Reverse();
+            List<T> bro = q2.q;
+            q1.q.InsertRange(q1.q.Count, bro);
+            return new Q<T> { };
+        }
+
+        public static Q<T> operator >(Q<T> q1, Q<T> q2)
+        {
+            return new Q<T> { };
+        }
+
+        public static bool operator false(Q<T> q1)
+        {
+            return q1.q.Count != 0;
+        }
+
+        public static bool operator true(Q<T> q1)
+        {
+            return q1.q.Count == 0;
+        }
+
         public List<T> AddElement(T element)
         {
             this.q.Insert(0, element);
@@ -25,6 +67,7 @@ namespace lab3
 
         public void ShowQ()
         {
+            this.q.Reverse();
             foreach (T x in this.q)
             {
                 Console.WriteLine(x);
@@ -42,6 +85,33 @@ namespace lab3
             q1.AddElement(3);
             Console.WriteLine("test 1");
             q1.ShowQ();
+            Q<int> q2 = new Q<int>();
+            for (int i = 3; i < 8; i++)
+            {
+                _ = q2 + i;
+            }
+            Console.WriteLine("testing + operator");
+            q2.ShowQ();
+            Console.WriteLine("testing -- operator");
+            --q2;
+            q2.ShowQ();
+            Console.WriteLine("testing < operator");
+            Q<int> q3 = new Q<int>();
+            _ = q3 < q2;
+            q3.ShowQ();
+            Console.WriteLine("testing true operator");
+            Q<int> q4 = new Q<int>();
+            if (q4)
+            {
+                Console.WriteLine("queue is empty");
+            }
+            else
+            {
+                Console.WriteLine("queue is not empty");
+            }
+            Q<byte> q5 = new Q<byte>();
+            q5.AddElement(3);
+            _ = (int)q5;
         }
     }
 }
